dcd87926e0b42269d2e7ae78f46ebdc785611c22
Jacob Marshall <JacobCMarshall1856@gmail.com>
2017-11-01 16:45:07 -0700

    Added a new setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#mkdir ~/.vim
+#mkdir ~/.vim/colors
+#cp ~/myVim/colors/jellybeans.vim ~/.vim/colors/
+echo "" >> ~/.bash_profile
+echo "###########################" >> ~/.bash_profile
+echo "# VIM ALIAS FOR JACOB MARSHALL" >> ~/.bash_profile
+echo "alias v=\"vim -u ~/myVim/.vimrc\"" >> ~/.bash_profile
+echo "###########################" >> ~/.bash_profile
