919ba8839a1d539a2eb973115821fa5ac3ccfe76
jacobchrismarsh <15932179+jacobchrismarsh@users.noreply.github.com>
2018-06-19 22:27:11 -0700

    Removed some comments

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-#mkdir ~/.vim
-#mkdir ~/.vim/colors
-#cp ~/myVim/colors/jellybeans.vim ~/.vim/colors/
+mkdir ~/.vim
+mkdir ~/.vim/colors
+cp ~/myVim/colors/jellybeans.vim ~/.vim/colors/
 echo "" >> ~/.bash_profile
 echo "###########################" >> ~/.bash_profile
 echo "# VIM ALIAS FOR JACOB MARSHALL" >> ~/.bash_profile
