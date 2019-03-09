fab6b8200a1251f33445bb634a4d645c9fbfe1c3
Jacob Marshall <Jacobcmarshall1856@gmail.com>
2019-03-08 22:57:36 -0800

    Had to account for downloading submodules

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
+echo "Installing vim plugins..."
+git submodule init && git submodule update
+
+echo "Copying Vim plugins to ~/.vim/bundle/"
 cp -R .vim  ~/.vim
 
+echo "Creating a new vim alias in ~/.bash_profile"
 echo "" >> ~/.bash_profile
 echo "###########################" >> ~/.bash_profile
 echo "# VIM ALIAS FOR JACOB MARSHALL" >> ~/.bash_profile
 echo "alias v=\"vim -u ~/myVim/.vimrc\"" >> ~/.bash_profile
 echo "###########################" >> ~/.bash_profile
+
+echo "Ready to go!"
